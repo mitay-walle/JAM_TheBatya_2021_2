8f43f610c9bb29c175126bb9f5b4ad01631202a8
mitay-walle <mitay-walle91@mail.ru>
2021-08-20 10:33:00 +0300

    TMP

---
+++ Assets/Plugins/StickToTransform.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class StickToTransform : MonoBehaviour
+{
+	public Transform StickTo;
+	public bool Rotation;
+	public bool Late;
+	
+	private void LateUpdate()
+	{
+		if (!Late) return;
+		
+		if (StickTo)
+		{
+			transform.position = StickTo.position;
+			if (Rotation) transform.rotation = StickTo.rotation;
+		}
+	}
+
+	void Update () 
+	{
+		if (Late) return;
+		if (StickTo)
+		{
+			transform.position = StickTo.position;
+			if (Rotation) transform.rotation = StickTo.rotation;
+		}
+	}
+}
