922ba4d8b8829438e65e404313091931dfbd84de
mitay-walle <mitay-walle91@mail.ru>
2021-08-21 11:21:45 +0300

    intro sounds wip, camera move fix

--- Assets/Scripts/Saver.cs
+++ Assets/Scripts/Saver.cs
@@ -6,17 +6,18 @@ using UnityEngine;
 public class Saver : MonoBehaviour
 {
     private const string INTRO = "INTRO";
+    public bool DebugIntro;
     public bool SkipIntro;
     [SerializeField] private Animated Finish;
     [SerializeField] private SwitchableGoParent _switch;
     [SerializeField] private int _skipIndex = 2;
-    
-    
+
+
     private void Awake()
     {
         Finish.OnPlayAction -= Save;
         Finish.OnPlayAction += Save;
-        
+
         Load();
     }
 
@@ -33,19 +34,25 @@ public class Saver : MonoBehaviour
 
         if (SkipIntro)
         {
+#if UNITY_EDITOR
+            if (DebugIntro) return;
+#endif
             _switch.Show(_skipIndex);
+        }
+        else
+        {
+            _switch.Show(0);
         }
     }
 
     public void Save()
     {
-        PlayerPrefs.SetInt(INTRO,1);
+        PlayerPrefs.SetInt(INTRO, 1);
     }
-    
+
     [Button]
     public void Clear()
     {
         PlayerPrefs.DeleteAll();
     }
-    
 }
